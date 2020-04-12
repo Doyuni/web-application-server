9009cdba393fc206402c7c0d9bd434bc7bce9770
doyuni <gdypc@naver.com>
2020-04-12 09:44:16 +0900

    요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -24,7 +27,30 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String url = HttpRequestUtils.getURL(br.readLine());
+            String firstLine = br.readLine();
+            String url = HttpRequestUtils.getURL(firstLine);
+
+
+            if (url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+                String requestPath = url.substring(0, index);
+                String params = url.substring(index + 1);
+                log.debug("Request Path: {}", requestPath);
+                log.debug("Params: {}", params);
+                Map<String, String> userData = HttpRequestUtils.parseQueryString(params);
+                String userId = userData.get("userId");
+                String password = userData.get("password");
+                String name = userData.get("name");
+                String email = userData.get("email");
+
+                User user = new User(userId, password, name, email);
+                log.debug("User object 확인: {}", user);
+
+                url = "/index.html";
+//                // 데이터 조회
+//                Stream<Map<String, String>> stringStream = Stream.of(userData);
+//                stringStream.forEach(System.out::println);
+            }
 
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
